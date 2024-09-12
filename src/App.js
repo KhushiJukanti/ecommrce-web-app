647bb2fdfd10b65a5f5ff558360bba7f1d1c0000
IK <khushijukanti33@gmail.com>
2024-09-12 23:05:29 +0530

    update app file with js components and pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import CartPage from './pages/CartPage';
+import AdminPage from './pages/AdminPage';
+
+const App = () => {
   return (
-    <div className="App">
-      Hi Khushi
-    </div>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
+

0f052f5fe77218c90b862c453ccf8407e6dc6080
IK <khushijukanti33@gmail.com>
2024-09-12 23:01:50 +0530

    update index file with authcontext

--- src/index.js
+++ src/index.js
@@ -7,10 +7,14 @@ import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import { AuthProvider } from './context/AuthContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
